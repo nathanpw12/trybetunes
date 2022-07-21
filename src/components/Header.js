cd1f89bc1bad26c8cb8a40a352f2eb6fd573f4a9
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-21 16:19:54 -0300

    Requisito 4: Aprovado pelo npm test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -27,6 +28,9 @@ class Header extends React.Component {
         <p data-testid="header-user-name">
           { loading ? <Loading /> : <h3>{ nameInput }</h3> }
         </p>
+        <Link to="/search" data-testid="link-to-search"> Search </Link>
+        <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
+        <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
       </header>
     );
   }
