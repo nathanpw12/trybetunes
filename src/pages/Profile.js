4e0525b52b210aa3adeff99a6247697ec736df7c
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-26 07:37:25 -0300

    Requisito 13: Aprovado pelo npm test

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        description: '',
+        email: '',
+        image: '',
+        name: '',
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.handleUser();
+  }
+
+  handleUser = async () => {
+    const user = await getUser();
+    console.log(user);
+    this.setState({
+      user,
+    });
+  }
+
   render() {
+    const { user } = this.state;
     return (
-      <div data-testid="page-profile">
-        <h1>Profile</h1>
+      <>
         <Header />
-      </div>
+        <div data-testid="page-profile">
+          <h2>
+            { user.name }
+          </h2>
+          <h2>
+            { user.description }
+          </h2>
+          <h2>
+            { user.email }
+          </h2>
+          <img data-testid="profile-image" src={ user.image } alt="perfil" />
+          <Link to="/profile/edit">Editar perfil</Link>
+        </div>
+      </>
     );
   }
 }
